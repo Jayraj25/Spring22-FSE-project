20321979ad056cd08cacedc5766ebcb96c55abdd
vivek <vivek>
2022-04-09 13:00:46 -0400

    DAO related items created for polls

---
+++ interfaces/PollDaoI.ts
@@ -0,0 +1,10 @@
+/**
+ * @file Declares DAO interface managing Data Storage Access for polls.
+ */
+
+import Poll from "../models/Poll";
+
+export default interface PollDaoI {
+    createPoll(uid: string, poll: Poll): Promise<Poll>;
+
+}
