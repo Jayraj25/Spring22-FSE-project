8d31f9fd2bef985fdd19f804aff73b7f6f6d8c76
vivek <vivek>
2022-04-15 02:03:49 -0400

    Delete polls DAO and DAO interface

--- daos/PollDao.ts
+++ daos/PollDao.ts
@@ -1,6 +1,7 @@
 import PollDaoI from "../interfaces/PollDaoI";
 import Poll from "../models/Poll";
 import PollModel from "../mongoose/PollModel";
+import TuitModel from "../mongoose/TuitModel";
 
 
 export default class PollDao implements PollDaoI {
@@ -29,6 +30,32 @@ export default class PollDao implements PollDaoI {
         return await PollModel.create({...poll, createdBy: uid});
     }
 
+    /**
+     * Finds all polls.
+     * @returns Promise To be notified when the polls are found.
+     */
+    async getAllPolls(): Promise<Poll[]> {
+        return PollModel.find();
+    }
+
+    /**
+     * Finds a poll by its primary key.
+     * @param {string} id Poll's primary key
+     * @returns Promise To be notified when the poll is found.
+     */
+    async getPollById(id: string): Promise<any> {
+        return PollModel.findById(id);
+    }
+
+
+    /**
+     * Deletes a poll by its primary key
+     * @param {string} pid poll's primary key
+     * @returns Promise To be notified regarding the status of the operation
+     */
+    async deletePoll(pid: string): Promise<any> {
+        return PollModel.deleteOne({_id: pid});
+    }
 
 
 }
